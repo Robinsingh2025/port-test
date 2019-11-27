a18bf25a829a763a951f11120f26120f4806abbc
Robin7374 <7374robin@gmail.com>
2019-11-27 13:04:24 -0700

    Go wrk

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./About.style.scss";
 export default function About() {
   return (
-    <div>hello</div>
+    <div className="container">
+      <div className="section">
+        <h1>Hi, I'm Robin.Good to meet you</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+          laborum unde hic voluptates doloremque vel praesentium, culpa eius
+          excepturi, distinctio debitis quo iusto nihil.
+        </p>
+      </div>
+    </div>
   );
 }
